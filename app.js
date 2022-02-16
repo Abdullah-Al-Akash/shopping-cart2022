58311e823ba51cefacd972a0655a39af99c2a314
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-02-16 22:55:22 +0600

    Phone Total Complete

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 // Update Case Number:
-function updateCaseNumber(isIncreasing) {
-        const caseInput = document.getElementById("case-number");
-        const caseNumber = caseInput.value;
+function updateCaseNumber(product, price, isIncreasing) {
+        const caseInput = document.getElementById(product + "-number");
+        let caseNumber = caseInput.value;
         if (isIncreasing) {
-                caseInput.value = parseInt(caseNumber) + 1;
+                caseNumber = parseInt(caseNumber) + 1;
         }
         else if (caseNumber > 0) {
-                caseInput.value = parseInt(caseNumber) - 1;
+                caseNumber = parseInt(caseNumber) - 1;
         }
+        caseInput.value = caseNumber;
+        // Update Case Total:
+        const caseTotal = document.getElementById(product + "-total");
+        caseTotal.innerText = caseNumber * price;
 }
+
+// Phone Increase:
+document.getElementById("phone-plus").addEventListener("click", function () {
+        updateCaseNumber('phone', 1219, true);
+})
+// Phone Decrease:
+document.getElementById("phone-minus").addEventListener("click", function () {
+        updateCaseNumber('phone', 1219, false);
+})
 
 // When Increase Case Number:
 document.getElementById("case-plus").addEventListener("click", function () {
-        updateCaseNumber(true);
+        updateCaseNumber('case', 59, true);
 })
 
 // When Decrease Case Number:
 document.getElementById("case-minus").addEventListener("click", function () {
-        updateCaseNumber(false);
+        updateCaseNumber('case', 59, false);
 })
