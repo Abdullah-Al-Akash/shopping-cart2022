379b63abaa39fbc562849a4e287cba1ba3c5cf42
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-02-16 23:00:34 +0600

    Update Product Number Function

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 // Update Case Number:
-function updateCaseNumber(product, price, isIncreasing) {
-        const caseInput = document.getElementById(product + "-number");
-        let caseNumber = caseInput.value;
+function updateProduct(product, price, isIncreasing) {
+        const productInput = document.getElementById(product + "-number");
+        let productNumber = productInput.value;
         if (isIncreasing) {
-                caseNumber = parseInt(caseNumber) + 1;
+                productNumber = parseInt(productNumber) + 1;
         }
-        else if (caseNumber > 0) {
-                caseNumber = parseInt(caseNumber) - 1;
+        else if (productNumber > 0) {
+                productNumber = parseInt(productNumber) - 1;
         }
-        caseInput.value = caseNumber;
+        productInput.value = productNumber;
         // Update Case Total:
-        const caseTotal = document.getElementById(product + "-total");
-        caseTotal.innerText = caseNumber * price;
+        const productTotal = document.getElementById(product + "-total");
+        productTotal.innerText = productNumber * price;
 }
 
 // Phone Increase:
 document.getElementById("phone-plus").addEventListener("click", function () {
-        updateCaseNumber('phone', 1219, true);
+        updateProduct('phone', 1219, true);
 })
 // Phone Decrease:
 document.getElementById("phone-minus").addEventListener("click", function () {
-        updateCaseNumber('phone', 1219, false);
+        updateProduct('phone', 1219, false);
 })
 
 // When Increase Case Number:
 document.getElementById("case-plus").addEventListener("click", function () {
-        updateCaseNumber('case', 59, true);
+        updateProduct('case', 59, true);
 })
 
 // When Decrease Case Number:
 document.getElementById("case-minus").addEventListener("click", function () {
-        updateCaseNumber('case', 59, false);
+        updateProduct('case', 59, false);
 })
