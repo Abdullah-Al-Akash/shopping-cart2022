a8e2447431cbc4b6b20c9f2a574ad784e800d5fb
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-02-16 22:17:23 +0600

    Update Case Number

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('I am in a separate file')
+// Update Case Number:
+function updateCaseNumber(isIncreasing) {
+        const caseInput = document.getElementById("case-number");
+        const caseNumber = caseInput.value;
+        if (isIncreasing) {
+                caseInput.value = parseInt(caseNumber) + 1;
+        }
+        else if (caseNumber > 0) {
+                caseInput.value = parseInt(caseNumber) - 1;
+        }
+}
+
+// When Increase Case Number:
+document.getElementById("case-plus").addEventListener("click", function () {
+        updateCaseNumber(true);
+})
+
+// When Decrease Case Number:
+document.getElementById("case-minus").addEventListener("click", function () {
+        updateCaseNumber(false);
+})
